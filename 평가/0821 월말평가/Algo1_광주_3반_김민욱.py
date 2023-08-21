43a352f0a55982d92d032a8aa13d05705b04fa98
minwookkim <minwookkim@naver.com>
2023-08-21 11:13:02 +0900

    0821

---
+++ 평가/0821 월말평가/Algo1_광주_3반_김민욱.py
@@ -0,0 +1,35 @@
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    balloon = [list(map(int, input().split())) for _ in range(N)]
+
+    max_v = 0  # 최대값
+    min_v = 10000  # 최소값
+
+    # 풍선을 돌면서
+    for r in range(N):
+        for c in range(N):
+
+            # 풍선을 터트렸을 때 점수
+            bomb = balloon[r][c]
+            # 상하좌우 돌면서
+            for k in range(4):
+                # 그 자리의 수만큼 터트림
+                for d in range(1, balloon[r][c] + 1):
+                    nr = r + dr[k] * d
+                    nc = c + dc[k] * d
+                    # 범위안에 있을 때
+                    if 0 <= nr < N and 0 <= nc < N:
+                        # 점수 더하기
+                        bomb += balloon[nr][nc]
+            # 최대값에 넣고
+            if max_v <= bomb:
+                max_v = bomb
+            # 최소값에 넣고
+            if min_v >= bomb:
+                min_v = bomb
+
+    print(f"#{tc} {max_v - min_v}")
---
+++ 평가/0821 월말평가/Algo2_광주_3반_김민욱.py
@@ -0,0 +1,39 @@
+T = int(input())
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    card = list(map(str, input().split()))
+
+    B_q = []  # B는 queue로 앞에서부터 뺌
+    C_st = []  # C는 stack으로 뒤에서부터 뺌
+
+    plus = 0  # 더하기의 개수로 보너스 숫자
+    for i in card:  # 카드 돌때마다
+        if i == "+":  # 더하기 만나면
+            plus += 1  # 보너스 숫자 + 1
+        else:  # 숫자면
+            a = int(i) + plus  # 보너스 숫자 더하기
+            if a % 2 == 1:  # 홀수면
+                B_q.append(a)  # B에
+            else:  # 짝수면
+                C_st.append(a)  # C에
+
+    answer = 0  # 김싸피가 M번째에 고른 답
+    # 김싸피가 점수를 획득하지 못하는 경우는
+    # B와 C모두 M번째로 고를 수가 없는 경우
+    # if len(B_q) < M and len(C_st) < M:
+    #     answer = 0
+    # # 고를수 있는 경우
+    # else:
+    for i in range(M):  # M번 돌면서
+        if len(B_q) == 0:  # 비어있으면
+            B = 0  # 0으로 계산
+        else:  # 아니면
+            B = B_q.pop(0)  # B는 앞에서 꺼냄
+        if len(C_st) == 0:  # 비어 있으면
+            C = 0  # 0으로 계산
+        else:  # 아니면
+            C = C_st.pop()  # C는 뒤에서 꺼냄
+
+        answer = B + C  # M번째에 나올 답
+
+    print(f"#{tc} {answer}")
