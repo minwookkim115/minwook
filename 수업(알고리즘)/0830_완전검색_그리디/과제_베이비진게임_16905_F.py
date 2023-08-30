16f42799c39aa4f090742cbacba2aeb1cc5bc7f7
김민욱 <minwookkim@naver.com>
2023-08-31 01:48:21 +0900

    0831

--- 수업(알고리즘)/0830_완전검색_그리디/과제_베이비진게임_16905_F.py
+++ 수업(알고리즘)/0830_완전검색_그리디/과제_베이비진게임_16905_F.py
@@ -1,12 +1,12 @@
 import sys
 sys.stdin = open("16905_input.txt", "r")
 
-def winner(i, a, b):
-    a.sort()
-    if i == 3:
-        return
-    else:
-        for j in range(3):
+# def winner(i, n ):
+#     if i == 3:
+#         return
+#     else:
+#         for j in range(3):a, b):
+#     a.sort()
 
 
 T = int(input())
@@ -21,11 +21,12 @@ for tc in range(1, T + 1):
 
     answer = 0
 
+
     for i in range(12):
         if i % 2 == 0:
             p1.append(card_l[i])
         else:
             p2.append(card_l[i])
 
-        if len(p1) >= 3:
-            winner(0, p1, p2)
+        # if len(p1) >= 3:
+        #     winner(0, p1, p2)
