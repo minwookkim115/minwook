d2bc9bbb0cfb9de86e5ff32cc2ece1f822c1d2d7
minwookkim <minwookkim@naver.com>
2023-09-01 17:53:11 +0900

    0901

--- 수업(알고리즘)/0831_완전검색_그리디/과제_최대상금_1244_teacher.py
+++ 수업(알고리즘)/0831_완전검색_그리디/과제_최대상금_1244_teacher.py
@@ -3,16 +3,19 @@ import sys
 sys.stdin = open("1244_input.txt", "r")
 
 
-def swap(cnt, c):
-    global max_v
+def swap(cnt):
+    global answer
+    val = int("".join(S))  # 문자열 배열 숫자로 만들기
+    if (cnt, val) in visited:  # cnt번 바꾸기 진행할건데 이전에 만든적이있는 숫자면 진행x
+        return
+
+    # 전에 만든적 없는 숫자면 만든적 있다고 체크한뒤 진행
+    visited.add((cnt, val))
+
     # 종료 조건 : 교환 횟수를 다 소모 했다면
     # 바꾼 결과물을 숫자로 바꿔서 최대상금 계산
     if cnt == N:
-        a = ""
-        for i in S:
-            a += i
-        if max_v < int(a):
-            max_v = int(a)
+        answer = max(answer, val)
         return
 
     # 자리를 바꿀 위치 2개를 교환 한번 할때마다 새로 지정
@@ -22,12 +25,9 @@ def swap(cnt, c):
     for i in range(len(S) - 1):
         for j in range(i + 1, len(S)):
             S[i], S[j] = S[j], S[i]
-            if S[0] == max(S):
-                swap(cnt + 1, c)
-                # 바꿨던거 원상 복구 시키고 새로운 i, j 지정
-                S[i], S[j] = S[j], S[i]
-            else:
-                S[i], S[j] = S[j], S[i]
+            swap(cnt + 1)
+            # 바꿨던거 원상 복구 시키고 새로운 i, j 지정
+            S[i], S[j] = S[j], S[i]
 
 
 T = int(input())
@@ -35,9 +35,9 @@ for tc in range(1, T + 1):
     S, N = input().split()
     S = list(S)
     N = int(N)
-    max_v = 0
-    check = []
 
-    swap(0, check)
+    visited = set()  # (k, num) : 자리를 k번 교환했을때 num을 만든적이 있다.
+    answer = 0
+    swap(0)
 
-    print(f"#{tc} {max_v}")
+    print(f"#{tc} {answer}")
