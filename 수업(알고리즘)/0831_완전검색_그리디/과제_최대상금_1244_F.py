c0b0809eeb00eb428fe184ef01a2651eb1917972
김민욱 <minwookkim@naver.com>
2023-09-04 20:36:14 +0900

    0904

--- 수업(알고리즘)/0831_완전검색_그리디/과제_최대상금_1244_F.py
+++ 수업(알고리즘)/0831_완전검색_그리디/과제_최대상금_1244_F.py
@@ -4,4 +4,3 @@ sys.stdin = open("1244_input.txt", "r")
 T = int(input())
 for tc in range(1, T + 1):
     num, c = map(int, input().split())
-
--- 수업(알고리즘)/0831_완전검색_그리디/과제_최대상금_1244_teacher.py
+++ 수업(알고리즘)/0831_완전검색_그리디/과제_최대상금_1244_teacher.py
@@ -3,7 +3,7 @@ import sys
 sys.stdin = open("1244_input.txt", "r")
 
 
-def swap(cnt, c):
+def swap(cnt):
     global max_v
     # 종료 조건 : 교환 횟수를 다 소모 했다면
     # 바꾼 결과물을 숫자로 바꿔서 최대상금 계산
@@ -22,12 +22,25 @@ def swap(cnt, c):
     for i in range(len(S) - 1):
         for j in range(i + 1, len(S)):
             S[i], S[j] = S[j], S[i]
-            if S[0] == max(S):
-                swap(cnt + 1, c)
-                # 바꿨던거 원상 복구 시키고 새로운 i, j 지정
-                S[i], S[j] = S[j], S[i]
+            if N >= 2:
+                if S[0] == max1:
+                    swap(cnt + 1)
+                    S[i], S[j], = S[j], S[i]
+                    if S[1] == max2:
+                        swap(cnt + 1)
+                        # 바꿨던거 원상 복구 시키고 새로운 i, j 지정
+
+                    else:
+                        S[i], S[j] = S[j], S[i]
+                else:
+                    S[i], S[j] = S[j], S[i]
             else:
-                S[i], S[j] = S[j], S[i]
+                if S[0] == max1:
+                    swap(cnt + 1)
+                    # 바꿨던거 원상 복구 시키고 새로운 i, j 지정
+                    return
+                else:
+                    S[i], S[j] = S[j], S[i]
 
 
 T = int(input())
@@ -36,8 +49,15 @@ for tc in range(1, T + 1):
     S = list(S)
     N = int(N)
     max_v = 0
-    check = []
 
-    swap(0, check)
+    max1 = max(S)
+    max2 = ""
+    for i in range(len(S)):
+        if S[i] == max(S):
+            S.pop(i)
+            max2 = max(S)
+            S.insert(i, max1)
+
+    swap(0)
 
     print(f"#{tc} {max_v}")
