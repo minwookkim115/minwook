711fcb93708a4e43ab9aff9b164520e28d215e47
minwookkim <minwookkim@naver.com>
2023-09-07 17:52:07 +0900

    0907

--- 자습/study/pr.py
+++ 자습/study/pr.py
@@ -1,42 +1,16 @@
-T = int(input())
-for tc in range(T):
-    N = int(input())
+N, M = map(int, input().split())
 
-    answer = 0
+num_l = []
+for i in range(1, N + 1):
+    num_l.append(i)
 
-    answer_l = []
+sl = []
+el = []
+for _ in range(M):
+    s, e = map(int, input().split())
+    sl.append(s)
+    el.append(e)
 
-    count = 0
-
-    for i in range(1, N+1):
-        if i == 1:
-            answer = 1
-            answer_l.append(1)
-        if i == 2:
-            answer = 2
-            answer_l.append(2)
-        if i == 3:
-            answer = 4
-            answer_l.append(4)
-        if i >= 4:
-            count = sum(answer_l)
-            answer = count
-            answer_l.append(count)
-            answer_l.pop(0)
-
-    print(answer)
-
-"""
-10
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-
-"""
+for i in range(M):
+    if sl[i] % 2 == 1:
+        for j in range(sl[i]-1, )
---
+++ 평가/일타싸피/일타싸피.py
@@ -0,0 +1,9 @@
+import math
+
+math.sqrt
+
+math.sin
+
+math.atan
+
+math.degrees(radian)
