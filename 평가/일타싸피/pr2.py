dfb646313dab943752a7837bbdf8739b5707778a
김민욱 <minwookkim@naver.com>
2023-09-10 23:11:21 +0900

    0910

--- 평가/일타싸피/pr2.py
+++ 평가/일타싸피/pr2.py
@@ -4,9 +4,9 @@ import math
 # B = 60
 # C = 90
 
-a = 1
-b = float(math.sqrt(3))
-c = 2
+a = 10
+b = 11
+c = 6
 
 
 # a**2 = b**2 + c**2 - 2bc * cosA
@@ -17,9 +17,13 @@ print(A1)
 
 # c**2 = a**2 + b**2 - 2ab * cos다
 # 다 = arccos(a**2 + b**2 - c**2 / 2ab)
-num1 = math.acos((a**2 + b**2 - c**2) / 2*a*b)
+num1 = math.acos((a**2 + c**2 - b**2) / (2*a*c))
 B1 = math.degrees(num1)
 print(B1)
+
+num2 = math.acos((a**2 + b**2 - c**2) / (2*a*b))
+C1 = math.degrees(num2)
+print(C1)
 
 # d**2 = a**2 + (b+2r)**2 - 2*a*(b+2r) * cos다
 
