c16527fc82f506c05b6fcc0f7935ce081fb7ffc9
kimminuk <minwookkim@naver.com>
2023-10-06 17:52:18 +0900

    1006

--- 자습/study/pr.py
+++ 자습/study/pr.py
@@ -1,2 +1,16 @@
-import sys
-sys.stdin = open('input.txt', 'r')
+# import sys
+# sys.stdin = open('input.txt', 'r')
+
+A, B, V = map(int, input().split())
+
+height = 0
+count = 1
+while True:
+    height += A
+    if height >= V:
+        break
+    else:
+        height -= B
+        count += 1
+
+print(count)
