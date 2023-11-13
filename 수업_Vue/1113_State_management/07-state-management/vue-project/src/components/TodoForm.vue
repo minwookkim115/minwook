d9b272d21ce949c4bb61fe4baf033d8d83c64df5
minwookkim <minwookkim@naver.com>
2023-11-13 12:08:01 +0900

    1113

---
+++ 수업_Vue/1113_State_management/07-state-management/vue-project/src/components/TodoForm.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<form @submit.prevent="createTodo()" ref="formElem">
+			<input type="text" v-model="todoText">
+			<input type="submit">
+		</form>
+	</div>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+import { ref } from 'vue'
+
+const todoText = ref('')
+const formElem = ref(null)
+
+const store = useCounterStore()
+
+const createTodo = function () {
+	store.addTodo(todoText.value)
+	formElem.value.reset()
+	// todoText.value = ''
+}
+</script>
+
+<style scoped>
+
+</style>
